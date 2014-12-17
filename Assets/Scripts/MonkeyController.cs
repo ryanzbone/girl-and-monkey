8598b15c2d0d7f03244ac374f3cd212ebc4129c1
Ryan Bone <ryanzbone@gmail.com>
2014-12-17 17:20:07 -0500

    create monkey controller for determining if in mount zone

---
+++ Assets/Scripts/MonkeyController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonkeyController : MonoBehaviour {
+
+    public bool inMountZone;
+    public bool InMountZone { get { return inMountZone; } }
+	// Use this for initialization
+	void Start () {
+        inMountZone = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        inMountZone = other.tag == "MountCollider";
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (inMountZone && other.tag == "MountCollider")
+            inMountZone = false;
+    }
+}
